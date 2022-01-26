f939998e2b04481ca4dcc712fc8d410b8e7ad9dd
Ghassen Bahroun <ghassenbahroun@yahoo.fr>
2022-01-26 15:17:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+          <h1 className="title red" style={{color:'red'}} >Your name here</h1>
+          <br />
+          <img src="/imageInSrc.jpg" />
+          <br />
+          <img src="/imageInPublic.jpg" />
+    </div>
+          <video width="320" height="240" controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+          </video>
+          <div>
+            <img src='.C:\createdApp\jsxchall\src\ImageInSrc.jpg' alt='profileimg'></img>
+          </div>
     </div>
   );
 }
